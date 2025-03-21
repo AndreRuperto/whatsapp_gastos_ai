49da09858447d2a99367f228a86ed87a0972c508
André Ruperto <andreruperto@gmail.com>
2025-03-21 16:16:26 -0300

    Corrigindo para importação correta

--- backend/services/cotacao_service.py
+++ backend/services/cotacao_service.py
@@ -5,8 +5,8 @@ import os
 
 logger = logging.getLogger(__name__)
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Obtém o diretório do script atual
-MOEDAS_FILE = os.path.join(BASE_DIR, "backend", "data", "moedas.json")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Diretório do script atual
+MOEDAS_FILE = os.path.join(BASE_DIR, "..", "data", "moedas.json")
 
 with open(MOEDAS_FILE, "r", encoding="utf-8") as file:
     dados_moedas = json.load(file)
